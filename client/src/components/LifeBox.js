476899bfd904e6850ac89f399429080e1c84c90c
warrgiff <warrenbruceg@gmail.com>
2020-09-10 17:44:47 -0700

    expansion of both the test react canvas (added control buttons) and also the lifebox component, still working on functioning react model

--- client/src/components/LifeBox.js
+++ client/src/components/LifeBox.js
@@ -2,80 +2,90 @@ import React, { useEffect } from 'react';
 import { render } from 'react-dom';
 
 const LifeBox = () => {
+
     useEffect(() => {
 
     })
-    const canvas = document.querySelector('canvas');
+
+    const canvas = document.querySelector('canvas')
     const ctx = canvas.getContext('2d')
 
-    const resolution = 40;
-    canvas.width = 400;
-    canvas.height = 400;
+    const resolution = 10;
+    canvas.width = 800;
+    canvas.height = 800;
 
     const COLS = canvas.width / resolution;
     const ROWS = canvas.height / resolution;
 
+    // makes randomly populated grid
     function buildGrid() {
         return new Array(COLS).fill(null)
             .map(() => new Array(COLS).fill(null)
-                .map(() => Math.floor(Math.random * 2)));
+                .map(() => Math.floor(Math.random() * 2)));
     }
 
-    let grid = buildGrid()
+    //renders lifebox to canvas
+    function makeLifeBox(grid) {
+        for (let col = 0; col < grid.length; col++) {
+            for (let row = 0; row < grid[col].length; row++) {
+                const cell = grid[col][row];
 
-    requestAnimationFrame(update)
-
-    function update() {
-        grid = nextGen(grid)
-        makeLifeBox(grid)
-        requestAnimationFrame(update)
+                ctx.beginPath();
+                ctx.rect(col * resolution, row * resolution, resolution, resolution)
+                ctx.fillStyle = cell ? 'black' : 'white';
+                ctx.fill();
+                ctx.stroke();
+            }
+        }
     }
 
-    function nextGen(grid) {
+    //make next interation based of rules of conways game of life
+
+    function nextGen(gird) {
         const nextGen = grid.map(arr => [...arr]);
 
         for (let col = 0; col < grid.length; col++) {
             for (let row = 0; row < grid[col].length; row++) {
-                const node = grid[col][row];
+                const cell = grid[col][row];
+                let numNeighbours = 0;
                 for (let i = -1; i < 2; i++) {
-                    for (let j = -1; j < -2; j++) {
-                        if (i === 0 && j === 0) continue;
-                    }
-                    const x_cell = col + i;
-                    const y_cell = row + j;
+                    for (let j = -1; j < 2; j++) {
+                        if (i === 0 && j === 0) {
+                            continue;
+                        }
+                        const x_cell = col + i;
+                        const y_cell = col + j;
 
-                    if (x_cell >= 0 && y_cell >= 0 && x_cell < COLS && y_cell < ROWS) {
-                        const currentNeighbour = grid[col + i][col + j];
-                        numNeighbour += currentNeighbour;
+                        if (x_cell >= 0 && y_cell >= 0 && x_cell < COLS && y_cell < ROWS) {
+                            const currentNeighbour = grid[col + i][row + j]
+                            numNeighbours += currentNeighbour
+                        }
                     }
                 }
-
-                //rules of conways game of life
-                if (node === 1 && numNeightbours < 2) {
-                    nextGen[col][row] = 0;
-                } else if (node === 1 && numNeighbour > 3) {
-                    nextGen[col][row] = 0;
-                } else if (node = 0 && numNeighbors === 3) {
-                    nextGen[col][row] = 1;
+                //rules
+                if (cell === 1 && numNeighbours < 2) {
+                    nextGen[col][row] = 0
+                } else if (cell === 1 && numNeighbours > 3) {
+                    nextGen[col][row] = 0
+                } else if (cell === 0 && numNeighbours === 3) {
+                    nextGen[col][row] = 1
                 }
             }
         }
         return nextGen
     }
 
-    function makeLifeBox() {
-        for (let col = 0; col < grid.length; col++) {
-            for (let row = 0; row < grid[col].length; row++) {
-                const node = grid[col][row];
-
-                ctx.beginPath();
-                ctx.rect(col * resolution, row * resolution, resolution, resolution)
-                ctx.fillstyle = node ? 'black' : 'white';
-                ctx.fill();
-                ctx.stroke();
-            }
-        }
+    //renders grid on every generation
+    function update() {
+        grid = nextGen(grid);
+        makeLifeBox(grid);
+        requestAnimationFrame(update);
     }
+
+
+    //execute functions to produce and render game of life
+    let grid = buildGrid();
+    requestAnimationFrame(update);
 
     return (
         <>
--- client/src/components/TestCanvas.js
+++ client/src/components/TestCanvas.js
@@ -16,18 +16,41 @@ function draw(ctx, location) {
 }
 
 function App() {
+    const [locations, setLocations] = React.useState([])
     const canvasRef = React.useRef(null)
+
+    React.useEffect(() => {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+        ctx.clearRect(0, 0, window.innerHeight, window.innerWidth)
+        locations.forEach(location => draw(ctx, location))
+    })
+
+    function handleCanvasClick(e) {
+        const newLocation = { x: e.clientX, y: e.clientY }
+        setLocations([...locations, newLocation])
+    }
+
+    function handleClear() {
+        setLocations([])
+    }
+
+    function handleUndo() {
+        setLocations(locations.slice(0, -1))
+    }
+
     return (
-        <canvas
-            ref={canvasRef}
-            width={window.innerWidth}
-            height={window.innerHeight}
-            onClick={e => {
-                const canvas = canvasRef.current
-                const ctx = canvas.getContext('2d')
-                draw(ctx, { x: e.clientX, y: e.clientY })
-            }}
-        />
+        <>
+            <button onClick={handleClear}>Clear</button>
+            <button onClick={handleUndo}>Undo</button>
+            <canvas
+                ref={canvasRef}
+                width={window.innerWidth}
+                height={window.innerHeight}
+                onClick={handleCanvasClick}
+            />
+        </>
     )
 }
+
 export default App
