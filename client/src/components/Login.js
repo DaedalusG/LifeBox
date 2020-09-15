4471809d28313513a2c025eca453935295c7fc88
warrgiff <warrenbruceg@gmail.com>
2020-09-15 08:56:17 -0700

    started work on modal setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import RandomLifeBox from './RandomLifeBox.js'
 import Glider from '../images/glider.js'
+import SignUpModal from './SignUpModal.js'
 import { apiUrl } from '../config.js'
 
 
@@ -32,6 +33,13 @@ const Login = () => {
         }
     };
 
+    const [signupModal, setSignup] = useState(false)
+
+    const handleSignUp = () => {
+        let toggleSignup = !signupModal;
+        setSignup(toggleSignup)
+    }
+
     return (
         <div className={"login_container"}>
             <div className={"login_form_container"}>
@@ -50,9 +58,11 @@ const Login = () => {
                     value={password}
                     onChange={updatePassword} />
                 <button className={"login_form_submit"} onClick={handleLoginSubmit}>Submit</button>
+                <div className={"signup_link"} onClick={handleSignUp}>Need an Account?</div>
+                <div className={"signup_link"}>Sign in as Demo</div>
             </div>
             <RandomLifeBox className={"login_background"} />
-        </div>
+        </div >
     )
 }
 
