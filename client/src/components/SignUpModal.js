f763892835743fdba22b18232af7a7158f7b6738
warrgiff <warrenbruceg@gmail.com>
2020-09-15 11:02:26 -0700

    signup modal portal working, wont overlay other materials

--- client/src/components/SignUpModal.js
+++ client/src/components/SignUpModal.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 
 const SignUpModal = ({ openSignUp, closeSignUp }) => {
     if (!openSignUp) return null
-    return (
+    return ReactDom.createPortal(
         <div>
             <button onClick={closeSignUp}>Close</button>
             <div>Test</div>
-        </div>
+        </div>,
+        document.getElementById('signupmodal')
     )
 }
 
