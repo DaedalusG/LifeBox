178f98a6513e706a9fed02f1c505632418bf5ddd
warrgiff <warrenbruceg@gmail.com>
2020-09-15 13:09:34 -0700

    setting up signup modal formating

--- client/src/components/SignUpModal.js
+++ client/src/components/SignUpModal.js
@@ -4,10 +4,18 @@ import ReactDom from 'react-dom'
 const SignUpModal = ({ openSignUp, closeSignUp }) => {
     if (!openSignUp) return null
     return ReactDom.createPortal(
-        <div>
-            <button onClick={closeSignUp}>Close</button>
-            <div>Test</div>
-        </div>,
+        <div className={"signupmodal"}>
+            <div>
+                <button onClick={closeSignUp}>Close</button>
+            </div>
+            <img src="../../public/default_profile_pic.png" alt="default_profile_pic"></img>
+            <input></input>
+            <input></input>
+            <input></input>
+            <div>
+                <button className={"register_button"}>Register</button>
+            </div>
+        </div >,
         document.getElementById('signupmodal')
     )
 }
