a5ebe835626b4cd0594922044bacff03f0d8538f
warrgiff <warrenbruceg@gmail.com>
2020-09-14 22:48:47 -0700

    added some ideas to the db seed

--- database.py
+++ database.py
@@ -14,6 +14,8 @@ with app.app_context():
     metal_fingers = User(username='metal_fingers', email='cutz@lair.io')
     ultron = User(username='ultron', email='robo-rights@lair.io')
     ElizabethII = User(username='ElizabethII', email='lizzy@england.io')
+    IggyPop = User(username='Iggy', email='Iggy@england.io')
+    xenomorph = User(username='xenomorph', email='queen@corp.com')
 
     db.session.add(ian)
     db.session.add(javier)
@@ -21,5 +23,6 @@ with app.app_context():
     db.session.add(metal_fingers)
     db.session.add(ultron)
     db.session.add(ElizabethII)
+    db.session.add(IggyPop)
 
     db.session.commit()
