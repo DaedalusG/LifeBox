c6cf101e4bcac62af59aee554c3af6dbb6ba0ff5
warrgiff <warrenbruceg@gmail.com>
2020-09-15 23:19:20 -0700

    cleaned up seed data in database.py, still not working

--- database.py
+++ database.py
@@ -3,61 +3,59 @@ from backend import app, db
 from dotenv import load_dotenv
 load_dotenv()
 
-password_digest: BCrypt: : Password.create('Your_Password'))
-
 with app.app_context():
     db.drop_all()
     db.create_all()
 
-    ian=User(
-        username = 'Ian',
-        email = 'ian@aa.io',
-        hashed_password = password_digest: BCrypt: : Password.create('Your_Password')),  # noqa
+    ian = User(
+        username='Ian',
+        email='ian@aa.io',
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    javier=User(
+    javier = User(
         username='Javier',
         email='javier@aa.io',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    dean=User(
+    dean = User(
         username='Dean',
         email='dean@aa.io',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    metal_fingers=User(
+    metal_fingers = User(
         username='metal_fingers',
         email='cutz@lair.io',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    ultron=User(
+    ultron = User(
         username='ultron',
         email='robo-rights@lair.io',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    ElizabethII=User(
+    ElizabethII = User(
         username='ElizabethII',
         email='lizzy@england.io',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    xenomorph=User(
+    xenomorph = User(
         username='xenomorph',
         email='queen@corp.com',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    IggyPop=User(
+    IggyPop = User(
         username='Iggy',
         email='Iggy@england.io',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    predator=User(
+    predator = User(
         username='ThePredator',
         email='@corp.com',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
-    Arnold=User(
+    Arnold = User(
         username='Arnold',
         email='arnold@corp.com',
-        hashed_password=$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC,  # noqa
+        hashed_password='$2b$12$vu6DPbq4MzCBwvlqjPiy1Oq0huYhoxlRWx5ipZ1AvEo8McV.QD1KC',  # noqa
     )
 
     db.session.add(ian)
