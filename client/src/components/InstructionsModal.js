9e012dbd1e376c08df92a2c34179253c135ad9d4
warrgiff <warrenbruceg@gmail.com>
2020-09-19 21:29:24 -0700

    made the thing draggable

--- client/src/components/InstructionsModal.js
+++ client/src/components/InstructionsModal.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
 import ReactDom from 'react-dom'
+import Draggable from 'react-draggable';
 import Close from '../images/close.js'
 
 const InstructionsModal = ({ openInstructions, closeInstructions }) => {
     if (!openInstructions) return null
 
     return ReactDom.createPortal(
-        <div className={"instructionsmodal"}>
-            <div className={'login_close_container'} onClick={closeInstructions}>
-                <Close />
+        <Draggable>
+            <div className={"instructionsmodal"}>
+                <div className={'login_close_container'} onClick={closeInstructions}>
+                    <Close />
+                </div>
             </div>
-        </div>,
+        </Draggable>,
         document.getElementById('instructionsmodal')
     )
 }
