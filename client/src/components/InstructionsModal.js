202bfedae1c57a29ce135b426f53db4683aafe26
warrgiff <warrenbruceg@gmail.com>
2020-09-20 02:26:11 -0700

    finished up instructions modal

--- client/src/components/InstructionsModal.js
+++ client/src/components/InstructionsModal.js
@@ -12,11 +12,24 @@ const InstructionsModal = ({ openInstructions, closeInstructions }) => {
             <div className={"instructionsmodal"}>
                 <div className={'instructions_sub_container_row'}>
                     <img id={'Conway'} src={Conway} alt="john_conway"></img>
-                    <div className={'instructions_sub_container_column'}>
-                        <div className={"instructions_close"} onClick={closeInstructions}>
-                            <Close />
+                    <div className={'instructions_sub_container_row'}>
+                        <div className={'instructions_sub_container_column'}>
+                            <div className={"instructions_close"}>
+                                <div>Conways Game of Life</div>
+                                <div onClick={closeInstructions}>
+                                    <Close />
+                                </div>
+                            </div>
+                            <div className={'quote'}>"I just thought if you couldn't predict what it did,</div>
+                            <div className={'quote'}>it could probably do anything"   <span id={'instructions_title'}>-John Conway</span></div>
+                            <p>
+                                <div className={'rule'}><span id={'instructions_title'}>Rule 1: </span>{' Any live cell with two or three live neighbours survives.'}</div>
+                                <div className={'rule'}><span id={'instructions_title'}>Rule 2: </span>{' Any dead cell with three live neighbours becomes a live cell.'}</div>
+                                <div className={'rule'}><span id={'instructions_title'}>Rule 3: </span>{' All other live cells die in the next generation. Similarly, all other dead cells stay dead.'}</div>
+                                <div className={'rule'}></div>
+                            </p>
+                            <div id={"instructions_title"} className={'quote'}>There is no way to win game of life, and it's questionable wetehr or not its a game.</div>
                         </div>
-                        <div> Rules Conway's Game of Life</div>
                     </div>
                 </div>
             </div>
