b470aaa1df62b4922a2ebf9d0447dc8d788d8feb
warrgiff <warrenbruceg@gmail.com>
2020-09-19 22:00:48 -0700

    working on instructions content

--- client/src/components/InstructionsModal.js
+++ client/src/components/InstructionsModal.js
@@ -12,6 +12,18 @@ const InstructionsModal = ({ openInstructions, closeInstructions }) => {
                 <div className={'login_close_container'} onClick={closeInstructions}>
                     <Close />
                 </div>
+                <div>
+                    <h1>Rule 1</h1>
+                    <p>Any live cell with two or three live neighbours survives.</p>
+                </div>
+                <div>
+                    <h1>Rule 2</h1>
+                    <p>Any dead cell with three live neighbours becomes a live cell.</p>
+                </div>
+                <div>
+                    <h1>Rule 3</h1>
+                    <p>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</p>
+                </div>
             </div>
         </Draggable>,
         document.getElementById('instructionsmodal')
