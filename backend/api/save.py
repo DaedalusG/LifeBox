a155140d1476abb1b24315b19cfa330c89e47806
warrgiff <warrenbruceg@gmail.com>
2020-09-27 18:30:19 -0700

    working on basic flask implementation for saving grid

--- backend/api/save.py
+++ backend/api/save.py
@@ -2,3 +2,15 @@ from flask import Blueprint, request, jsonify
 from backend.models import db, User
 
 save = Blueprint('save', __name__)
+
+
+@save.route('/saving', methods=['GET, POST'])
+def saving():
+    data = request.get_json()
+
+    # try:
+    #     name = data['name']
+    #     grid = data['grid']
+
+    #     print(data)
+    return jsonify(message='hit route')
