9b85c9538bb5fb1068201d9eeb1ef16ea09f9399
warrgiff <warrenbruceg@gmail.com>
2020-10-03 23:41:51 -0700

    slightly improved fluidity of render updates

--- client/src/components/DrawLife.js
+++ client/src/components/DrawLife.js
@@ -109,17 +109,13 @@ const DrawLife = (props) => {
     // generates iterations of the lifebox
     useEffect(() => {
         if (!generate) return
-        if (resolution === 10) {
-            requestAnimationFrame(
-
-            )
-        }
-        setTimeout(() => {
-            genCount.current++
-            setGrid(nextGen(grid))
-        }, genFreq)
-        ctx.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-        renderLifeBox();
+        requestAnimationFrame(runGeneration)
+        // setTimeout(() => {
+        //     genCount.current++
+        //     setGrid(nextGen(grid))
+        // }, genFreq)
+        // ctx.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+        // renderLifeBox();
     })
 
     //---------------functions--------------
