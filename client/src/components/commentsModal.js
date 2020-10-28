694106ff6ae4cbf8e00825fda508f48a2f0251ec
warrgiff <warrenbruceg@gmail.com>
2020-10-27 22:38:17 -0700

    styling bocx for comments field

--- client/src/components/commentsModal.js
+++ client/src/components/commentsModal.js
@@ -9,7 +9,7 @@ const CommentsModal = ({ }) => {
 
     return ReactDom.createPortal(
         <div className="commentsmodal">
-            <img src={Conway} alt="john_conway" />
+            {/* <img src={Conway} alt="john_conway" /> */}
         </div>,
         document.getElementById('commentsmodal')
     )
