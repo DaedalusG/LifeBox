9ebaf3b9fd1557c23dbdb9baced1e43993086a63
warrgiff <warrenbruceg@gmail.com>
2020-10-28 23:46:47 -0700

    working on fetching owner info for loaded grid

--- client/src/components/commentsModal.js
+++ client/src/components/commentsModal.js
@@ -1,15 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDom from 'react-dom'
+import { apiUrl } from '../config.js'
 import Comment from '../images/comment.svg'
 
 const CommentsModal = ({ loadGrid }) => {
     const [openComment, setComment] = useState(false)
-    if (loadGrid.name === undefined) return null
+    const [gridOwner, setOwner] = useState(null)
+
+    useEffect(() => {
+        const getOwner = async () => {
+            const token = window.localStorage.getItem('auth_token')
+            let response = await fetch(`${apiUrl}/grids/comment_info`, {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    name: loadGrid.owner
+                })
+            })
+
+            const res = await response.json()
+            setOwner(res)
+        }
+        getOwner()
+    }, [loadGrid])
 
     const toggleComment = () => {
         if (openComment === false) { setComment(true) } else { setComment(false) }
     }
 
+    if (loadGrid.name === undefined) return null
     return ReactDom.createPortal(
         <>
             <div className="comments_icon_container">
